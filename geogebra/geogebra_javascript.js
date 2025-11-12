ce29d38951807163a192c5007f16c4a6653ba782
jgrosf <jgrosf00@estudiantes.unileon.es>
2025-11-12 01:50:33 +0100

    Update geogebra_javascript.js

--- geogebra/geogebra_javascript.js
+++ geogebra/geogebra_javascript.js
@@ -33,8 +33,8 @@ function esperarAppletYActivar() {
 function inicializarListeners(ggbApplet) {
     console.log("✅ GeoGebra listo. Listeners activados.");  
   	window.fuerzaActualizacionF1 = fuerzaActualizacionF1;
-	ggbApplet.registerObjectUpdateListener("actualizacionDominioF1", "fuerzaActualizacionF1");
-	ggbApplet.registerObjectUpdateListener("actualizacionFuncion1", "fuerzaActualizacionF1");
+	ggbApplet.registerObjectUpdateListener("listaDominioDefinicionF1", "fuerzaActualizacionF1");
+	ggbApplet.registerObjectUpdateListener("f_{1}", "fuerzaActualizacionF1");
     ggbApplet.registerObjectUpdateListener("actualizacion1", "fuerzaActualizacionF1");
     ggbApplet.registerObjectUpdateListener("actualizacion2", "fuerzaActualizacionF2");
     ggbApplet.registerObjectUpdateListener("actualizacion3", "fuerzaActualizacionF3");
@@ -80,8 +80,8 @@ function actualizaAdvertencia() {
 function fuerzaActualizacionF1() {
     //alert("EUREKA GLOBAL F1");
 	console.log("🔄 actualizacion1 ha cambiado:", ggbApplet.getValue("actualizacion1"));
-	ggbApplet.unregisterObjectUpdateListener("actualizacionDominioF1");
-	ggbApplet.unregisterObjectUpdateListener("actualizacionFuncion1");
+	ggbApplet.unregisterObjectUpdateListener("listaDominioDefinicionF1");
+ggbApplet.unregisterObjectUpdateListener("f_{1}");
     ggbApplet.unregisterObjectUpdateListener("actualizacion1");
     ggbApplet.unregisterObjectUpdateListener("actualizacion2");
     ggbApplet.unregisterObjectUpdateListener("actualizacion3");
@@ -404,8 +404,8 @@ function fuerzaActualizacionF1() {
     ggbApplet.setValue("actualizacionDominioF1", 0);
     ggbApplet.setValue("actualizacionFuncion1", 0);
     ggbApplet.evalCommand("limites=true");
-	ggbApplet.registerObjectUpdateListener("actualizacionDominioF1", "fuerzaActualizacionF1");
-	ggbApplet.registerObjectUpdateListener("actualizacionFuncion1", "fuerzaActualizacionF1");
+	ggbApplet.registerObjectUpdateListener("listaDominioDefinicionF1", "fuerzaActualizacionF1");
+	ggbApplet.registerObjectUpdateListener("f_{1}", "fuerzaActualizacionF1");
     ggbApplet.registerObjectUpdateListener("actualizacion1", "fuerzaActualizacionF1");
     ggbApplet.registerObjectUpdateListener("actualizacion2", "fuerzaActualizacionF2");
     ggbApplet.registerObjectUpdateListener("actualizacion3", "fuerzaActualizacionF3");
